e522850e853ebcc1ecc6406cfea13d12cf828cda
Tomas <tslemenson@gmail.com>
2019-05-09 08:25:19 -0700

    popup style change

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -155,7 +155,7 @@ function displayResults(trackNum, name, trackNames, followers, genre, photo, spo
     popup.html('<p class="more_info" >More Info</p>');
     popup.append(popUpSpan);
     
-    popUpSpan.html('<div class="card mb-3 text-dark text-center"><img src="' + photo + '" class="card-img-top" alt="Artist Photo"><div class="card-body"><h5 class="card-title">' + name + '</h5><div class="card-text">Spotify ID: ' + spotifyId + '</div><div class="card-text">Genre: ' + genre + '</div><div class="card-text">Followers: ' + followers + '</div></div>');
+    popUpSpan.html('<div class="card-container"><img src="' + photo + '" class="image-popup" alt="Artist Photo"><div class="artist_name">' + name + '</div><div class="spotify_id">Spotify ID: <span>' + spotifyId + '</span></div><div class="genre">Genre: <span>' + genre + '</span></div><div class="followers">Followers: <span>' + followers + '</span></div></div>');
     newRow.append(number, newArtist, newTrackName, newLabel, popup);
 
     $("#artist-data-table").append(newRow);
